fa14bb9820a970f5d2bacdf64cbf264d821ae58f
HabibRh26 <remondc26@gmail.com>
2019-11-05 19:01:10 -0800

    network lab8 and object detection text

---
+++ networkLab/networkLab8_cw/cnLab8/shah.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<winsock2.h>
+
+using namespace std;
+
+int main()
+{
+    WSADATA WSAData;
+    SOCKET server;
+    SOCKADDR_IN addr;
+
+    WSAStartup(MAKEWORD(2,0), &WSAData);
+    server = socket(AF_INET, SOCK_STREAM, 0);
+
+    addr.sin_addr.s_addr = inet_addr("192.168.1.13");
+    addr.sin_family=AF_INET;
+    addr.sin_port=htons(5555);
+
+    connect(server,(SOCKADDR *)&addr, sizeof(addr));
+    cout<<"Connected to server!"<<endl;
+
+    char buffer[1024]={'h', 'e', 'l', 'l', 'o', '.'};
+    send(server,buffer,sizeof(buffer),0);
+    cout<<"message sent"<<endl;
+
+    closesocket(server);
+    WSACleanup();
+    cout<<"Socket closed."<<endl<<endl;
+
+
+
+}
